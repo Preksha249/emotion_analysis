d1a8c55fa250b4cedaaede1054bb2d0f7719deed
Preksha_Shridhar <60837958+Preksha249@users.noreply.github.com>
2024-09-11 14:17:25 -0400

    Update Sentiment.jsx

--- src/components/Sentiment.jsx
+++ src/components/Sentiment.jsx
@@ -10,23 +10,43 @@ const Sentiment = () => {
     };
 
     const handleSubmit = async (e) => {
-        // e.preventDefault();
+        e.preventDefault();
 
+        // Set the sentence as the text to be processed
+        // setText(sentence);
         console.log(sentence)
+        if (!sentence) {
+            console.error("Text is empty or undefined");
+            return;
+        }
 
-        // const response = await fetch('http://localhost:5000/process-text', {
-        //   method: 'POST',
-        //   headers: {
-        //     'Content-Type': 'application/json',
-        //   },
-        //   body: JSON.stringify({ text }),
-        // });
-
-        // const result = await response.json();
-        // setProcessedText(result.processed_text);
+        try {
+            // Call the FastAPI endpoint
+            const response = await fetch('http://127.0.0.1:8000/analyze', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ text: sentence }),
+            });
+    
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+    
+            const result = await response.json();
+            console.log("result",result)
+    
+            // Set the processed text to state
+            setProcessedText(result.classification);
+            
+        } catch (error) {
+            console.error("There was an error with the fetch request:", error);
+        }
     };
-
+    console.log(processedText);
     return (
+        <div>
         <div className="p-20 bg-white rounded-lg shadow-md">
             <h1 className="text-2xl font-bold mb-4">Sentence Storage</h1>
             <form onSubmit={handleSubmit}>
@@ -38,6 +58,8 @@ const Sentiment = () => {
                 /><button type='submit'>Submit</button>
             </form>
         </div >
+        <a>{processedText}</a>
+        </div>
     )
 }
 
